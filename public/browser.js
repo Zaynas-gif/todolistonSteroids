21f4173a66c01c856dc79e6beb34e58a48df1c8f
Zaynas <45643593+Lolikas12@users.noreply.github.com>
2020-11-05 10:16:36 +0100

    Edit uppdate

--- public/browser.js
+++ public/browser.js
@@ -15,7 +15,7 @@ document.addEventListener("click", function(e){
     //if element with class "edit-me" are clicked  
 if (e.target.classList.contains("edit-me")) {
     //make sure that value did not disappear then button "cancel" is clicked.
-let userImput = prompt("Enter your desired new text", e.target.parentelement.parentElement.querySelector(".item-text").innerHTML)
+let userImput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
  //
  if (userImput) {
     axios.post('/uppdate-item', {text: userImput, id: e.target.getAttribute("data-id")}).then(function (){
