c2d2edab8c1b549faee7d32e07c3ac8f392a75fa
Jonas <zilvis9508@gmail.com>
2020-11-09 16:55:19 +0100

    pushed to Heroku

--- public/browser.js
+++ public/browser.js
@@ -47,13 +47,13 @@ document.addEventListener("click", function(e){
     }
     //Uppdate Feature
     if (e.target.classList.contains("edit-me")) {
-        let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
-        if (userInput) {
-          axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function () {
-            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
-          }).catch(function() {
-            console.log("Please try again later.")
-          })
-        }
+      let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+      if (userInput) {
+        axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function () {
+          e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+        }).catch(function() {
+          console.log("Please try again later.")
+        })
       }
-    })
+    }
+  })
